6a5769d0cf4d079162f23584271db36697932bae
James <103224608+o7q@users.noreply.github.com>
2025-03-14 10:10:09 -0500

    Improve tv overlay

--- _projectfiles/cat_model/renders/seq2gif_png_256x256_12.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Generating GIF...
-
-ffmpeg -y -i "sequence/%%04d.png" -vf "scale=256:256,palettegen=reserve_transparent=1:max_colors=32" "palette.png"
-ffmpeg -framerate 12 -y -i "sequence/%%04d.png" -i "palette.png" -lavfi "scale=256:256,paletteuse=alpha_threshold=128" -gifflags -offsetting "256x256_12.gif"
-
-title Done!
-rem pause
--- _projectfiles/james_stuff_text/renders/seq2gif_png_800x345_24.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Generating GIF...
-
-ffmpeg -y -i "sequence/%%04d.png" -vf "scale=800:345,palettegen=reserve_transparent=1" "palette.png"
-ffmpeg -framerate 24 -y -i "sequence/%%04d.png" -i "palette.png" -lavfi "scale=800:345,paletteuse=alpha_threshold=128" -gifflags -offsetting -loop -1 "render.gif"
-
-title Done!
-rem pause
